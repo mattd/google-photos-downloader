12fb611dfe3d0e7ed046e5b3fe209d61dbe50aa1
Matt Dawson <matthewtdawson@gmail.com>
2019-07-28 09:32:34 -0400

    Simplify the entry point

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const request = require('request-promise');
+const authorize = require('./lib/authorize');
+const credentials = require('./secrets/credentials');
+const download = require('./lib/download');
 
-const { authorize } = require('./auth');
-const credentials = require('./credentials');
-
-const getPhotos = auth => {
-    request.get('https://photoslibrary.googleapis.com/v1/mediaItems', {
-        auth: {
-            bearer: auth.credentials.access_token
-        }
-    }).then(response => {
-        console.log(response);
-    }).catch(err => {
-        console.log(err);
-    });
-};
-
-authorize(credentials, getPhotos);
+authorize(credentials, download);
