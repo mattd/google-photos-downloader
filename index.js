c66478f2f061104a51dcb5e599057f8bc71b17c0
Matt Dawson <matthewtdawson@gmail.com>
2019-08-08 15:57:24 -0400

    Configure prettier

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { authorize } = require('./lib/authorize');
-const credentials = require('./secrets/credentials');
-const { downloadMedia } = require('./lib/download-media');
+const { authorize } = require("./lib/authorize");
+const credentials = require("./secrets/credentials");
+const { downloadMedia } = require("./lib/download-media");
 
 authorize(credentials, downloadMedia);
